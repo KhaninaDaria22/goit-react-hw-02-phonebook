ec609034b294d306f7343e524a8dde4062f5f797
Daria Khanina <khanina_dasha@ukr.net>
2024-01-25 00:11:45 +0200

    commit3

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -4,7 +4,7 @@ import { Label, Form, Input, Button } from "./Phonebook.styled";
 export class Phonebook extends Component {
     state = {
         name: '',
-        numver: '',
+        number: '',
     };
 //відповідає за оновлення стану 
     handleNameChange = e => {
@@ -40,6 +40,8 @@ export class Phonebook extends Component {
                     type="text"
                     name="name"
                     value={name}
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                     onChange={this.handleNameChange}
                     required
                   />
@@ -50,6 +52,7 @@ export class Phonebook extends Component {
                     type="tel"
                     name="number"
                     value={number}
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                     required
                     onChange={this.handleNameChange}/>
